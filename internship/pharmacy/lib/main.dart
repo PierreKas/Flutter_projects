9c90a8f24abc2504be9c45e465d477191cdd7b5d
pierrekas <chikukaspierre@gmail.com>
2024-09-27 16:29:42 +0200

    last

--- internship/pharmacy/lib/main.dart
+++ internship/pharmacy/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:pharmacy/pages/Introductionpages/onboardingscreen.dart';
-import 'package:pharmacy/pages/home.dart';
 import 'package:pharmacy/services/notificationsService.dart';
 
 void main() async {
